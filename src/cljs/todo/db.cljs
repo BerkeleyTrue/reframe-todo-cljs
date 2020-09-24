be49b6868ba22e7979c025a4ecaf92de2c833dc1
Berkeley True <berkeleytrue@users.noreply.github.com>
2020-09-24 16:10:20 -0700

    feat: add todos ui

--- src/cljs/todo/db.cljs
+++ src/cljs/todo/db.cljs
@@ -1,4 +1,10 @@
 (ns todo.db)
 
 (def default-db
-  {:title "Todo App"})
+  {
+   :title "Todo App"
+   :todos [{:title "Do CET"
+            :is-completed? true}
+
+           {:title "Do snapraid build"
+            :is-completed? false}]})
--- src/cljs/todo/views.cljs
+++ src/cljs/todo/views.cljs
@@ -4,13 +4,29 @@
    [todo.subs :as subs]))
 
 
+(defn Todo [todo]
+  [:li {:className (str
+                     "todo"
+                     (if (:completed? todo) " completed" ""))
+        :key (:title todo)}
+    [:div
+      (:title todo)]])
+
+(defn Todos [todos]
+  [:div {:className "todos-wrap"}
+    [:ul {:className "todos"}
+      (map Todo todos)]])
+
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
+  (let [name (re-frame/subscribe [::subs/name])
+        todos (re-frame/subscribe [::subs/todos])]
+
     [:div
       [:header
         [:h1 @name]
         [:h2 "What needs to be done"]]
       [:main
         [:div {:className "input-wrap"}
+          (Todos @todos)
           [:label "Todo"
             [:input {:className "todo-input"}]]]]]))
